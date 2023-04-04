40972f582cf420e681dedec0c89686bc549eed7f
Yahya Mlaouhi <57452767+yahyamlaouhi@users.noreply.github.com>
2023-04-04 01:49:54 +0100

    add a comment to see how pipline is working

--- src/components/AddRepoForm.jsx
+++ src/components/AddRepoForm.jsx
@@ -7,6 +7,7 @@ import api from "../lib/apiGraphQL";
 import {isValidRepoUrl} from "../lib/util";
 import {repoStatusCode} from "../lib/repoStatusCode";
 import {ErrorMessage} from "../styles/Typography";
+// this to see how the pipline is working
 
 function AddRepoForm({goalsId, onGoalAdded, goals}) {
   const urlRef = useRef(null);
